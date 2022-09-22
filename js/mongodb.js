0494f2a3968db42e404dc5aa60e7d2a77eb3d3ab
Bryam Talledo <bryamtalledog@gmail.com>
2022-09-21 22:38:27 -0500

    Update

--- js/mongodb.js
+++ js/mongodb.js
@@ -1,7 +1,5 @@
 // Crear un documento
 // Documentos -> Objetos de tipo JSON
-db.mongo.insertOne({ nombre: 'Jesus', correo: 'bryam@mongo.com', edad: 40, estatus: 'A' })
-
 user2 = { nombre: 'Jesus', correo: 'bryam@mongo.com', edad: 40, estatus: 'A' }
 
 users = [
@@ -14,9 +12,3 @@ users = [
   { nombre: 'Bojack', correo: 'bojack@mongo.com', edad: 59, estatus: 'I' },
   { nombre: 'Dayane', correo: 'dayane@mongo.com', edad: 35, estatus: 'A' },
 ]
-
-db.users.insertMany([
-  { nombre: 'Tod', correo: 'tod@mongo.com', edad: 23, estatus: 'A' },
-  { nombre: 'Maria', correo: 'maria@mongo.com', edad: 12, estatus: 'A' },
-  { nombre: 'Pepe', correo: 'pepe@mongo.com', edad: 53, estatus: 'I' },
-])
