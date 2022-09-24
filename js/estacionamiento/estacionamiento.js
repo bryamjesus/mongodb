e890b84361f253efb879fa0dcc8fb8428105b812
Bryam Talledo <bryamtalledog@gmail.com>
2022-09-23 20:26:28 -0500

    Problem 8

--- js/estacionamiento/estacionamiento.js
+++ js/estacionamiento/estacionamiento.js
@@ -31,6 +31,10 @@ db.propietarios.find({ 'vehiculos.marca': 'Toyota' }, { _id: false, nombres: tru
 // Listar  los nombres, apellidos, departamento de propietarios,
 // así como los números de placa, marca y color de sus vehículos,
 // pero únicamente de los propietarios de sexo Femenino
-db.propietarios.find({ sexo: 'F' }, { _id: false, nombres: true, apellidos: true, departamento: true,'vehiculos.placa': true,'vehiculos.marca': true,'vehiculos.color': true});
+db.propietarios.find({ sexo: 'F' }, { _id: false, nombres: true, apellidos: true, departamento: true, 'vehiculos.placa': true, 'vehiculos.marca': true, 'vehiculos.color': true });
 
-// 
+
+// Listar sólo los números de placa de los vehículos así 
+// como los nombres y apellidos de propietarios de sexo Masculino 
+// que tienen vehículos de marca “Toyota” color “Blanco”
+db.propietarios.find({sexo: 'M', 'vehiculos.marca': 'Toyota','vehiculos.color': 'Blanco'}, { 'vehiculos.placa': true , _id: false, nombres: true, apellidos: true })
