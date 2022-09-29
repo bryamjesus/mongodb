3964d779df6f45c9e16566e27b9bf0cb0e5eb0ad
Bryam Talledo <bryamtalledog@gmail.com>
2022-09-28 22:43:23 -0500

    Update README.js

--- js/zips/README.md
+++ js/zips/README.md
@@ -1,2 +1,18 @@
 # EJERCICIO
 Descargar los [DEV TOOLS](https://www.mongodb.com/try/download/database-tools?tck=docs_databasetools) de mongo 
+
+## Problemas
+Todas las soluciones los puede encontrar en [``solucion.js``](solucion.js)
+1. Obtener una lista que muestre la cantidad total de la población por estado
+
+2. Obtener una lista que muestre la cantidad total de la población por estado, ordenado de mayor a menor cantidad de población
+3. Obtener una lista que muestre los 7 estados con mayor población
+4. Obtener una lista que muestre la cantidad total de población por estado ordenado de menor mayor cantidad de población
+5. Obtener una lista de los estados con una población mayor a 10 millones de habitantes
+6. Obtener una lista de los estados con una población mayor a 10 millones de habitantes ordenador de mayor a menor
+7. Obtener una lista que muestre la suma de la población de habitantes por ciudad en cada estado
+8. Obtener una lista que muestre la suma de la población de habitantes por ciudad en el esado “CA”
+9. Obtener una lista que muestre la suma de la población de habitantes en la ciudad de San Francisco
+10. Obtener una lista que muestre la suma de la población de habitantes en la ciudad de CUSHMAN
+11. Obtener una lista que muestre la suma de la población de habitantes en la ciudad de CUSHMAN en el estado MA
+12. Obtener una lista que muestre el promedio de habitantes por ciudad en cada estado
--- js/zips/solucion.js
+++ js/zips/solucion.js
@@ -1,10 +1,4 @@
-const x = {
-  _id: '01001',
-  city: 'AGAWAM',
-  loc: [-72.622739, 42.070206],
-  pop: 15338,
-  state: 'MA'
-}
+
 
 // Obtener una lista que muestre la cantidad total 
 // de la población por estado
@@ -64,9 +58,76 @@ db.zips.aggregate([
 ])
 
 
-// Obtener una lista que muestre el 
-// promedio de habitantes por ciudad en cada estado
+// Obtener una lista que muestre la suma 
+// de la población de habitantes por ciudad en cada estado
 db.zips.aggregate([
-  { $group: { _id: "$city", promEstado: { $avg: "$pop" } } },
+  {
+    $group: {
+      _id: { state: "$state", city: "$city" },
+      popSum: { $sum: "$pop" }
+    }
+  },
 ])
+
+
+// Obtener una lista que muestre la suma de la 
+// población de habitantes por ciudad en el esado “CA”
+db.zips.aggregate([
+  {
+    $group: {
+      _id: { state: "$state", city: "$city" },
+      popSum: { $sum: "$pop" }
+    }
+  },
+  { $match: { "_id.state": "CA" } }
+])
+
+
+// Obtener una lista que muestre la suma de la población 
+// de habitantes en la ciudad de San Francisco
+db.zips.aggregate([
+  {
+    $group: {
+      _id: { state: "$state", city: "$city" },
+      popSum: { $sum: "$pop" }
+    }
+  },
+  { $match: { "_id.city": "SAN FRANCISCO" } }
+])
+
+
+// Obtener una lista que muestre la suma de 
+//la población de habitantes en la ciudad de CUSHMAN
+db.zips.aggregate([
+  {
+    $group: {
+      _id: { state: "$state", city: "$city" },
+      popSum: { $sum: "$pop" }
+    }
+  },
+  { $match: { "_id.city": "CUSHMAN" } }
+])
+
+
+// Obtener una lista que muestre la suma de la población 
+// de habitantes en la ciudad de CUSHMAN en el estado MA
+/* 
+SELECT state, city, SUM(pop) AS popSum FROM zips GroupBy state, 
+city WHERE city='SAN FRANCISCO' AND state='MA'
+*/
+db.zips.aggregate([
+  {
+    $group: {
+      _id: { state: "$state", city: "$city" },
+      popSum: { $sum: "$pop" }
+    }
+  },
+  { $match: { "_id.city": "CUSHMAN", "_id.state": "MA", } }
+])
+
+
+// Obtener una lista que muestre el promedio
+// de habitantes por ciudad en cada estado.
+
+
 
